7ad843357257fd276bad4e40b7ff1ee918d99645
Tim Lawrence <tlawrence@ukcloud.com>
2016-10-18 15:52:17 +0100

    travis environment update

--- spec/fixtures/set_host.py
+++ spec/fixtures/set_host.py
@@ -3,7 +3,11 @@ import sys
 from python_hosts import Hosts, HostsEntry
 
 hosts = Hosts(path='/etc/hosts')
-hosts.remove_all_matching(name='bastion')
+try:
+  hosts.remove_all_matching(name='bastion')
+except:
+  print("bastion host does not exist in host file")
+
 new_entry = HostsEntry(entry_type='ipv4', address=sys.argv[1], names=['bastion'])
 hosts.add([new_entry])
 hosts.write()

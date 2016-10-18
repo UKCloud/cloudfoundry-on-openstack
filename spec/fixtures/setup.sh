fbbff55acea21d0927714fe994f8a983433686b7
Tim Lawrence <tlawrence@ukcloud.com>
2016-10-18 15:07:26 +0100

    travis environment update

--- spec/fixtures/setup.sh
+++ spec/fixtures/setup.sh
@@ -4,10 +4,6 @@ echo "IN SETUP SCRIPT"
 
 echo "INSTALLING OPENSTACK CLI"
 
-pip install python-openstackclient
-pip install python-heatclient
-pip install python-keystoneclient
-pip install python-hosts
 
 
 openstack --version
